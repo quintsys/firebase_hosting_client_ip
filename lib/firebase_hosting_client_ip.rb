6eb41a431c19fe96bac56072bcff6bb5f579b258
Erich N Quintero <qbantek@gmail.com>
2025-12-12 14:19:07 -0500

    Scaffold gem

---
+++ lib/firebase_hosting_client_ip.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "firebase_hosting_client_ip/version"
+
+module FirebaseHostingClientIp
+  class Error < StandardError; end
+  # Your code goes here...
+end
